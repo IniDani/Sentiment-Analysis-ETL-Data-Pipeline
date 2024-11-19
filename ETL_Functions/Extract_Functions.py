ff493f6789a15307d7efdc78eee11e79c0f834dd
IniDani <msaarafli007@gmail.com>
2024-11-19 17:56:16 +0700

    feat: Add extract functions

---
+++ ETL_Functions/Extract_Functions.py
@@ -0,0 +1,58 @@
+# Function to Extract Comments from a YouTube video using YouTube API
+def Extract_YT_Comments(video_id, youtube):
+  comments = []
+  next_page_token = None
+
+  # Video Title
+  video_request = youtube.videos().list(
+    part = 'snippet',
+    id = video_id
+  )
+  video_response = video_request.execute()
+  video_title = video_response['items'][0]['snippet']['title']
+
+  # Video Comments and Username
+  while True:
+    request = youtube.commentThreads().list(
+        part = 'snippet',
+        videoId = video_id,
+        pageToken = next_page_token,
+        maxResults = 100
+    )
+    response = request.execute()
+
+    for item in response['items']:
+      comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+      username = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
+      comments.append((comment, username, video_title))
+
+    next_page_token = response.get('nextPageToken')
+    if not next_page_token:
+      break
+
+  return comments
+
+
+
+# Function to Extract Comments from a Reddit Post
+def Extract_Reddit_Comments(post_url, reddit_client):
+  # Initialize a list to store the data
+  comments_data = []
+
+  # Get the submission (post) from the provided URL
+  submission = reddit_client.submission(url = post_url)
+
+  # Extract the post title
+  post_title = submission.title
+
+  # Replace "More Comments" to simplify and fetch all comments
+  submission.comments.replace_more(limit = 0)
+
+  # Iterate through each comment and collect the data
+  for comment in submission.comments.list():
+    # Extract the comment text, post title, and username
+    comment = comment.body
+    username = comment.author.name if comment.author else "Anonymous"
+    comments_data.append((comment, username, post_title))
+
+  return comments_data
---
+++ src/YouTube_DAG.py
@@ -0,0 +1,5 @@
+from googleapiclient.discovery import build
+from bs4 import BeautifulSoup
+
+from src.Credentials import YOUTUBE_API_KEY
+from src.Credentials import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT
